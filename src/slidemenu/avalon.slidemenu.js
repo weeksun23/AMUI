dcfccf2f855a027053d740b990a80846f502600a
Lich <jy013018@aliyun.com>
2015-07-22 15:32:43 +0800

    update

--- src/slidemenu/avalon.slidemenu.js
+++ src/slidemenu/avalon.slidemenu.js
@@ -2,9 +2,26 @@
 	侧边区域滑动插件，目前只支持由下向上的滑动
 	依赖base.js中的Util对象
 */
-define(["avalon","css!./avalon.slidemenu.css"],function(avalon){
+define(["avalon","css!./avalon.slidemenu.css?dd"],function(avalon){
 	//所有滑动区域共享的遮罩
 	var slideMask;
+	var support = {
+		animationend : (function(){
+			var el = document.createElement('div');
+			var transEndEventNames = {
+				WebkitAnimation: 'webkitAnimationEnd',
+				MozAnimation: 'animationend',
+				OAnimation: 'oAnimationEnd oanimationend',
+				animation: 'animationend'
+			};
+			for (var name in transEndEventNames) {
+				if (el.style[name] !== undefined) {
+					return transEndEventNames[name];
+				}
+			}
+			return false;
+		})()
+	};
 	var widget = avalon.ui.slidemenu = function(element, data, vmodels){
 		function hideMask(){
 			//点击遮罩 隐藏当前显示的slidemenu
@@ -19,7 +36,9 @@ define(["avalon","css!./avalon.slidemenu.css"],function(avalon){
 			slideMask.className = 'slidemask';
 			//tap mask 隐藏
 			avalon.bind(slideMask,"tap",hideMask);
-			avalon.bind(slideMask,'animationend',function(){
+			avalon.bind(slideMask,support.animationend,function(e){
+				e.preventDefault();
+				e.stopPropagation();
 				if(avalon(this).hasClass("slidemask-hide-animate")){
 					this.style.display = 'none';
 				}
@@ -36,7 +55,9 @@ define(["avalon","css!./avalon.slidemenu.css"],function(avalon){
 					element.innerHTML = "<div class='slidemenu-inner'>" + element.innerHTML + "</div>";
 				}
 				//为inner绑定动画结束事件
-				avalon.bind(element.querySelector("div.slidemenu-inner"),'animationend',function(){
+				avalon.bind(element.querySelector("div.slidemenu-inner"),support.animationend,function(e){
+					e.preventDefault();
+					e.stopPropagation();
 					if(avalon(this).hasClass("slidemenu-"+vmodel.position+"-hide-animate")){
 						this.parentNode.style.display = 'none';
 					}else{
@@ -48,6 +69,8 @@ define(["avalon","css!./avalon.slidemenu.css"],function(avalon){
 			vm.show = function(){
 				var inner = element.querySelector("div.slidemenu-inner");
 				element.style.display = 'block';
+				element.offsetWidth;
+				inner.offsetWidth;
 				inner.className = "slidemenu-inner slidemenu-"+vmodel.position+"-animate";
 				slideMask.style.display = 'block';
 				slideMask.className = 'slidemask slidemask-animate';
